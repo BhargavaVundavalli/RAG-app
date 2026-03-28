b1dc3c0e6897c727d3534d1705acf869c2abcd84
Bar-guv <bhargav.vundavalli@digipen.edu>
2026-03-28 13:17:26 +0530

    Made some slight changes
    Libraries were updated. Downgraded module related to text-2-text, module is less than version 5

--- finalappjson.py
+++ finalappjson.py
@@ -6,11 +6,16 @@ from pathlib import Path
 from langchain_community.document_loaders import JSONLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+# from langchain.chains import create_stuff_documents_chain
+# from langchain.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+
 
 # -----------------------
 # Load NVIDIA API Key
